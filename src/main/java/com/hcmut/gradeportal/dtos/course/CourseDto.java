13e40f1346aea3236bc705d40447a0ba027a36f9
Viet <viet.trankhmtbk22@hcmut.edu.vn>
2024-11-14 16:52:59 +0700

    Update attribute for course entity

--- src/main/java/com/hcmut/gradeportal/dtos/course/CourseDto.java
+++ src/main/java/com/hcmut/gradeportal/dtos/course/CourseDto.java
@@ -1,8 +1,13 @@
 package com.hcmut.gradeportal.dtos.course;
 
 public record CourseDto(
-        String courseCode,
-        String courseName,
-        Integer credit) {
+                String courseCode,
+                String courseName,
+                Integer credit,
+                Integer coefficient_of_TN,
+                Integer coefficient_of_BT,
+                Integer coefficient_of_BTL,
+                Integer coefficient_of_GK,
+                Integer coefficient_of_CK) {
 
 }
