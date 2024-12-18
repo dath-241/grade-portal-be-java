c0097543e70e824ed5eb5bf2e7ffec9aa369e5f1
Viet <viet.trankhmtbk22@hcmut.edu.vn>
2024-12-18 23:25:40 +0700

    update api

--- src/main/java/com/hcmut/gradeportal/config/SecurityConfig.java
+++ src/main/java/com/hcmut/gradeportal/config/SecurityConfig.java
@@ -18,56 +18,59 @@ import org.springframework.lang.NonNull;
 @Configuration
 public class SecurityConfig {
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+        private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
-        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-    }
+        public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+                this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true)
-                        .maxAge(3600);
-            }
-        };
-    }
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+                return new WebMvcConfigurer() {
+                        @Override
+                        public void addCorsMappings(@NonNull CorsRegistry registry) {
+                                registry.addMapping("/**")
+                                                .allowedOrigins("http://localhost:3000")
+                                                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                                                .allowedHeaders("*")
+                                                .allowCredentials(true)
+                                                .maxAge(3600);
+                        }
+                };
+        }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable) // Vô hiệu hóa CSRF (REST API không cần CSRF)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Stateless
-                .authorizeHttpRequests(auth -> auth
-                        // Các endpoint không yêu cầu xác thực
-                        .requestMatchers("/", "/health_check", "/login", "/error", "/oauth2/authorization/google",
-                                "/auth/login", "/admin/auth/login")
-                        .permitAll()
-                        // OAuth2 login
-                        .requestMatchers("/login/redirect", "/login/error").permitAll()
-                        // Các endpoint yêu cầu xác thực và phân quyền
-                        .requestMatchers("/admin/**", "/init-data").hasAuthority("ADMIN")
-                        .requestMatchers("/teacher/**").hasAnyAuthority("ADMIN", "TEACHER")
-                        .requestMatchers("/student/**").hasAnyAuthority("ADMIN", "STUDENT")
-                        .requestMatchers("/hall-of-fame",
-                                "/hall-of-fame/get-all")
-                        .hasAnyAuthority("ADMIN", "STUDENT", "TEACHER")
-                        // Các request còn lại cần xác thực
-                        .anyRequest().authenticated())
-                .oauth2Login(oauth2 -> oauth2
-                        .defaultSuccessUrl("/login/redirect", true)
-                        .failureUrl("/login/error"))
-                // Thêm JwtAuthenticationFilter vào chuỗi filter
-                .exceptionHandling(exception -> exception
-                        .accessDeniedHandler(new CustomAccessDeniedHandler()))
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http
+                                .csrf(AbstractHttpConfigurer::disable) // Vô hiệu hóa CSRF (REST API không cần CSRF)
+                                .sessionManagement(session -> session
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Stateless
+                                .authorizeHttpRequests(auth -> auth
+                                                // Các endpoint không yêu cầu xác thực
+                                                .requestMatchers("/", "/health_check", "/login", "/error",
+                                                                "/oauth2/authorization/google",
+                                                                "/auth/login", "/admin/auth/login",
+                                                                "/student/auth/login", "/teacher/auth/login")
+                                                .permitAll()
+                                                // OAuth2 login
+                                                .requestMatchers("/login/redirect", "/login/error").permitAll()
+                                                // Các endpoint yêu cầu xác thực và phân quyền
+                                                .requestMatchers("/admin/**", "/init-data").hasAuthority("ADMIN")
+                                                .requestMatchers("/teacher/**").hasAnyAuthority("ADMIN", "TEACHER")
+                                                .requestMatchers("/student/**").hasAnyAuthority("ADMIN", "STUDENT")
+                                                .requestMatchers("/hall-of-fame",
+                                                                "/hall-of-fame/get-all")
+                                                .hasAnyAuthority("ADMIN", "STUDENT", "TEACHER")
+                                                // Các request còn lại cần xác thực
+                                                .anyRequest().authenticated())
+                                .oauth2Login(oauth2 -> oauth2
+                                                .defaultSuccessUrl("/login/redirect", true)
+                                                .failureUrl("/login/error"))
+                                // Thêm JwtAuthenticationFilter vào chuỗi filter
+                                .exceptionHandling(exception -> exception
+                                                .accessDeniedHandler(new CustomAccessDeniedHandler()))
+                                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
-        return http.build();
-    }
+                return http.build();
+        }
 }
